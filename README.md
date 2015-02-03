1114f447b57c8bb9fd654b91cf45a3d9c25227dd
Dennis Kuhberg <dennis.kuhberg@gmail.com>
2015-02-04 00:31:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Fleet Overview [PHP + MySQL]
+
+- - - - - - - - - -
+INSTALLATION
+- - - - - - - - - -
+1. Import the "star-citizen-fleet.sql" from "import" to your database
+2. Open "index.php" from "upload" and edit line 15 to connect to your database
+3. Optional: If you changed the table name, search and replace `star-citizen-fleet`
+4. Upload all files from "upload" to your webspace - maybe "fleet" or else
+5. You're done and the script should show you this: http://misc.bofb.org/fleet/
+
+- - - - - - - - - -
+MODIFICATION
+- - - - - - - - - -
+If you want to edit the ship count:
+Connect to the database with your prefered MySQL tool like phpMyAdmin or else.
+Search the table for your ship and/or variant and edit the "quantity" field.
+
+- - - - - - - - - -
+CHANGELOG
+- - - - - - - - - -
+Will follow when the script got updated ;-)
+
+- - - - - - - - - -
+QUESTIONS
+- - - - - - - - - -
+RSI Thread: https://forums.robertsspaceindustries.com/discussion/228249/fleet-overview-php-mysql
+
+
+See you in the verse
+bergkuh
---
+++ upload/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<title>bofb.org Fleet</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="shortcut icon" href="img/favicon.ico" />
+</head>
+
+<body>
+<div id="fleet">
+<?php
+$db = @new mysqli('localhost', 'root', '', 'star-citizen-fleet');
+if (mysqli_connect_errno() == 0) {
+	$ships = 'SELECT `ship`, SUM(`quantity`) `total` FROM `star-citizen-fleet` WHERE `quantity` != 0 GROUP BY `ship` ORDER BY `ship` ASC';
+	$ships = $db->query($ships);
+	echo '<ul id="ships">';	
+	$overall = 'SELECT SUM(`quantity`) quantity, SUM(`quantity` * `price`) amount FROM `star-citizen-fleet` WHERE `quantity` != 0';
+	$overall = $db->query($overall);
+	while ($row = $overall->fetch_object()) {
+		echo '<li class="ship overall">';
+		echo '<div class="overlay"></div>';
+		echo '<div class="title">';
+		echo 'This ORG<br />has <span class="ships">' .$row->quantity. '</span> ships and<br />pledged <span class="pledge">$' .number_format($row->amount). '</span>';
+		echo '</div>';
+		echo '</li>';
+	}	
+	while ($row = $ships->fetch_object()) {		
+		$ship_css = $row->ship;
+		$ship_css = preg_replace("/\s+/", " ", $ship_css);
+		$ship_css = str_replace(" ", "_", $ship_css);
+		$ship_css = preg_replace("/[^A-Za-z0-9_]/","",$ship_css);
+		$ship_css = strtolower($ship_css);		
+		echo '<li class="ship" style="background-image: url(img/ships/' .$ship_css. '.jpg)">';
+		echo '<div class="overlay"></div>';
+		echo '<div class="title">';
+		echo '<span class="quantity">' .str_pad($row->total, 2, '0', STR_PAD_LEFT). '<span class="cross">x</span></span><span class="ship">' .$row->ship. '</span>';
+		$variants = 'SELECT `variant`, `quantity` FROM `star-citizen-fleet` WHERE `ship` = "' .$row->ship. '" AND `variant` != "NONE" ORDER BY `variant` ASC';
+		$variants = $db->query($variants);
+		if ($variants->num_rows >= 1) {
+			echo '<div class="info"></div>';		
+			echo '<ul id="variants">';
+			while ($row = $variants->fetch_object()) {
+				echo '<li>';
+				echo '<span class="quantity">' .str_pad($row->quantity, 2, '0', STR_PAD_LEFT). '</span><span class="cross">x</span><span class="variant">' .$row->variant. '</span>';
+				echo '</li>';
+			}
+			echo '</ul>';
+		}
+		$variants->close();		
+		echo '</div>';
+		echo '</li>';
+	}
+	echo '</ul>';
+	$ships->close();
+} else {
+	echo 'Error: <strong>' .mysqli_connect_errno(). ' : ' .mysqli_connect_error(). '</strong>';
+}
+$db->close();
+?>
+</div>
+</body>
+
+</html>
